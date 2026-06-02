de29a1013fb168cb50a110dbcf8b029401790978
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:29:13 +0530

    correction

--- Module 3-Core Java/Q34_ModuleDemo.java
+++ Module 3-Core Java/Q34_ModuleDemo.java
@@ -1,1 +1,11 @@
-public class Q34_ModuleDemo{public static void main(String[]a){System.out.println("Java Modules");}}
+class MessageUtil {
+    public static String getMessage() {
+        return "Hello from Utility Module!";
+    }
+}
+
+public class Q34_ModuleDemo {
+
+    public static void main(String[] args) {
+        System.out.println(MessageUtil.getMessage());
+    }
