88734d0b70430657bad5d0d3df03d7bb74fe8218
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:46:42 +0530

    add file

--- Module 3-Core Java/Q16_PalindromeChecker.java
+++ Module 3-Core Java/Q16_PalindromeChecker.java
@@ -1,1 +1,23 @@
-public class Q16_PalindromeChecker{public static void main(String[]a){String s="madam";String r=new StringBuilder(s).reverse().toString();System.out.println(s.equals(r));}}
+import java.util.Scanner;
+
+public class Q16_PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        // remove non-alphanumeric and convert to lowercase
+        String cleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+
+        if (cleaned.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+    }
+}
