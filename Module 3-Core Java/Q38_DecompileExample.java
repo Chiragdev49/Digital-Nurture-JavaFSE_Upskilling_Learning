f0c31f27d0ab62fca1861f7ed2da901f102a71b7
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:18:01 +0530

    correction

--- Module 3-Core Java/Q38_DecompileExample.java
+++ Module 3-Core Java/Q38_DecompileExample.java
@@ -1,1 +1,13 @@
-public class Q38_DecompileExample{public static void main(String[]a){System.out.println("Decompiler Demo");}}
+public class Q38_DecompileExample {
+
+    private int number = 10;
+
+    public int square(int n) {
+        return n * n;
+    }
+
+    public static void main(String[] args) {
+        Q38_DecompileExample obj = new Q38_DecompileExample();
+        System.out.println("Square of 5 = " + obj.square(5));
+    }
+}
