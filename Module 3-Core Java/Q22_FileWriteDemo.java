551f746fd3a78917a6e446c073de406b4e9ec913
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:40:31 +0530

    add file

--- Module 3-Core Java/Q22_FileWriteDemo.java
+++ Module 3-Core Java/Q22_FileWriteDemo.java
@@ -1,1 +1,26 @@
-import java.io.*; public class Q22_FileWriteDemo{public static void main(String[]a)throws Exception{new FileWriter("output.txt").write("Hello");}}
+import java.io.*;
+import java.util.Scanner;
+
+public class Q22_FileWriteDemo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter text: ");
+            String text = sc.nextLine();
+
+            FileWriter writer = new FileWriter("output.txt");
+            writer.write(text);
+            writer.close();
+
+            System.out.println("Data written successfully to output.txt");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        sc.close();
+    }
+}
