52334bee37080934bd068e2ecd48498e8f277b8b
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:55:48 +0530

    add file

--- Module 3-Core Java/Q02_SimpleCalculator.java
+++ Module 3-Core Java/Q02_SimpleCalculator.java
@@ -1,1 +1,44 @@
-import java.util.*; public class Q02_SimpleCalculator{public static void main(String[]a){Scanner s=new Scanner(System.in);double x=s.nextDouble(),y=s.nextDouble();System.out.println(x+y);System.out.println(x-y);System.out.println(x*y);System.out.println(y!=0?x/y:"Divide by zero");}}
+import java.util.Scanner;
+
+public class Q02_SimpleCalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double x = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double y = sc.nextDouble();
+
+        System.out.print("Choose operation (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        switch (op) {
+            case '+':
+                System.out.println("Result: " + (x + y));
+                break;
+
+            case '-':
+                System.out.println("Result: " + (x - y));
+                break;
+
+            case '*':
+                System.out.println("Result: " + (x * y));
+                break;
+
+            case '/':
+                if (y != 0) {
+                    System.out.println("Result: " + (x / y));
+                } else {
+                    System.out.println("Cannot divide by zero");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operation");
+        }
+
+        sc.close();
+    }
+}
