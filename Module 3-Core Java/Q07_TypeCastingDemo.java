18ceb18729cede0fd2738e99a77519ac73996389
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:52:28 +0530

    add file

--- Module 3-Core Java/Q07_TypeCastingDemo.java
+++ Module 3-Core Java/Q07_TypeCastingDemo.java
@@ -1,1 +1,17 @@
-public class Q07_TypeCastingDemo{public static void main(String[]a){double d=12.8;int i=(int)d;double d2=i;System.out.println(i+" "+d2);}}
+public class Q07_TypeCastingDemo {
+    public static void main(String[] args) {
+
+        // double to int (narrowing conversion)
+        double d = 12.8;
+        int i = (int) d;
+
+        // int to double (widening conversion)
+        int x = 10;
+        double d2 = x;
+
+        System.out.println("Double value: " + d);
+        System.out.println("After casting to int: " + i);
+        System.out.println("Int value: " + x);
+        System.out.println("After casting to double: " + d2);
+    }
+}
