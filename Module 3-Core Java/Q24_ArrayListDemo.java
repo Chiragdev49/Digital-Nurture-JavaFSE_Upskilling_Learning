f35091a395bade0c62431451ddd46eff4044d924
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:38:52 +0530

    add file

--- Module 3-Core Java/Q24_ArrayListDemo.java
+++ Module 3-Core Java/Q24_ArrayListDemo.java
@@ -1,1 +1,26 @@
-import java.util.*; public class Q24_ArrayListDemo{public static void main(String[]a){ArrayList<String> l=new ArrayList<>();l.add("A");System.out.println(l);}}
+import java.util.*;
+
+public class Q24_ArrayListDemo {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many students? ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Enter name " + i + ": ");
+            names.add(sc.nextLine());
+        }
+
+        System.out.println("\nStudent Names:");
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        sc.close();
+    }
+}
