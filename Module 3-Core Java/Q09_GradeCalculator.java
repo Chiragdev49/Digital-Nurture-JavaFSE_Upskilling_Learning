d45ee62f000638a38db2d82cfa32e7e20f1b7bb9
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:51:06 +0530

    add file

--- Module 3-Core Java/Q09_GradeCalculator.java
+++ Module 3-Core Java/Q09_GradeCalculator.java
@@ -1,1 +1,27 @@
-import java.util.*; public class Q09_GradeCalculator{public static void main(String[]a){int m=new Scanner(System.in).nextInt();System.out.println(m>=90?"A":m>=80?"B":m>=70?"C":m>=60?"D":"F");}}
+import java.util.Scanner;
+
+public class Q09_GradeCalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter marks out of 100: ");
+        int m = sc.nextInt();
+
+        if (m >= 90 && m <= 100) {
+            System.out.println("Grade: A");
+        } else if (m >= 80) {
+            System.out.println("Grade: B");
+        } else if (m >= 70) {
+            System.out.println("Grade: C");
+        } else if (m >= 60) {
+            System.out.println("Grade: D");
+        } else if (m >= 0) {
+            System.out.println("Grade: F");
+        } else {
+            System.out.println("Invalid marks");
+        }
+
+        sc.close();
+    }
+}
