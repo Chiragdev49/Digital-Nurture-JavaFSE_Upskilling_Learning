38f57c6301f5f9c518987403d3b0fbceb2efa012
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:49:08 +0530

    add file

--- Module 3-Core Java/Q12_MethodOverloading.java
+++ Module 3-Core Java/Q12_MethodOverloading.java
@@ -1,1 +1,23 @@
-public class Q12_MethodOverloading{static int add(int a,int b){return a+b;} static double add(double a,double b){return a+b;} static int add(int a,int b,int c){return a+b+c;} public static void main(String[]a){}}
+public class Q12_MethodOverloading {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+
+    static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Sum of 2 integers: " + add(10, 20));
+
+        System.out.println("Sum of 2 doubles: " + add(10.5, 20.5));
+
+        System.out.println("Sum of 3 integers: " + add(5, 10, 15));
+    }
+}
