4f5d45f56062c9bf0b077501c5db273289158caf
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:39:45 +0530

    file add

--- Module 3-Core Java/Q23_FileReadDemo.java
+++ Module 3-Core Java/Q23_FileReadDemo.java
@@ -1,1 +1,15 @@
-import java.nio.file.*; public class Q23_FileReadDemo{public static void main(String[]a)throws Exception{System.out.println(Files.readString(Path.of("output.txt")));}}
+import java.nio.file.*;
+import java.util.List;
+
+public class Q23_FileReadDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        List<String> lines =
+                Files.readAllLines(Path.of("output.txt"));
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
