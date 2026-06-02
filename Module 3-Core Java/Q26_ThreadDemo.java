9038fa60779d359f6c814d7ceef22f309e12fe42
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:37:16 +0530

    add file

--- Module 3-Core Java/Q26_ThreadDemo.java
+++ Module 3-Core Java/Q26_ThreadDemo.java
@@ -1,1 +1,21 @@
-class T extends Thread{public void run(){System.out.println("Running");}} public class Q26_ThreadDemo{public static void main(String[]a){new T().start();new T().start();}}
+class T extends Thread {
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName()
+                    + " : Running " + i);
+        }
+    }
+}
+
+public class Q26_ThreadDemo {
+
+    public static void main(String[] args) {
+
+        T t1 = new T();
+        T t2 = new T();
+
+        t1.start();
+        t2.start();
+    }
+}
