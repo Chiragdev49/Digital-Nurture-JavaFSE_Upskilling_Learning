2f48e8ffc8c86b604d33b8e27e6850c47365b365
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:16:27 +0530

    correction

--- Module 3-Core Java/Q40_VirtualThreadDemo.java
+++ Module 3-Core Java/Q40_VirtualThreadDemo.java
@@ -1,1 +1,47 @@
-public class Q40_VirtualThreadDemo{public static void main(String[]a){System.out.println("Virtual Threads Java 21");}}
+public class Q40_VirtualThreadDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        long startVirtual = System.currentTimeMillis();
+
+        Thread[] virtualThreads = new Thread[100000];
+
+        for (int i = 0; i < 100000; i++) {
+            int id = i;
+            virtualThreads[i] = Thread.startVirtualThread(() -> {
+                System.out.println("Virtual Thread " + id);
+            });
+        }
+
+        for (Thread t : virtualThreads) {
+            t.join();
+        }
+
+        long endVirtual = System.currentTimeMillis();
+
+        System.out.println("Virtual Threads Time: "
+                + (endVirtual - startVirtual) + " ms");
+
+        // Traditional Thread Comparison (reduced count)
+        long startTraditional = System.currentTimeMillis();
+
+        Thread[] normalThreads = new Thread[1000];
+
+        for (int i = 0; i < 1000; i++) {
+            int id = i;
+            normalThreads[i] = new Thread(() -> {
+                System.out.println("Traditional Thread " + id);
+            });
+            normalThreads[i].start();
+        }
+
+        for (Thread t : normalThreads) {
+            t.join();
+        }
+
+        long endTraditional = System.currentTimeMillis();
+
+        System.out.println("Traditional Threads Time: "
+                + (endTraditional - startTraditional) + " ms");
+    }
+}
