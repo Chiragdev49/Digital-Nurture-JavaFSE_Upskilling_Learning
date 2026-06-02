189aae14688390fb3488b477529ecdaea6c759b3
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:45:56 +0530

    add file

--- Module 3-Core Java/Q17_ClassAndObjectCreation.java
+++ Module 3-Core Java/Q17_ClassAndObjectCreation.java
@@ -1,1 +1,23 @@
-public class Q17_Car{String make,model;int year;void displayDetails(){System.out.println(make+model+year);}}
+public class Q17_Car {
+    String make, model;
+    int year;
+
+    void displayDetails() {
+        System.out.println(make + " " + model + " " + year);
+    }
+
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        c1.make = "Toyota";
+        c1.model = "Innova";
+        c1.year = 2022;
+
+        Car c2 = new Car();
+        c2.make = "Honda";
+        c2.model = "City";
+        c2.year = 2021;
+
+        c1.displayDetails();
+        c2.displayDetails();
+    }
+}
