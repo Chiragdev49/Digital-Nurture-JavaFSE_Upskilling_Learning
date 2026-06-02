bb14b8856d570d8c99eaba58d189f5417b7dbdd9
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:47:23 +0530

    add file

--- Module 3-Core Java/Q15_StringReversal.java
+++ Module 3-Core Java/Q15_StringReversal.java
@@ -1,1 +1,16 @@
-public class Q15_StringReversal{public static void main(String[]a){String s="Java";System.out.println(new StringBuilder(s).reverse());}}
+import java.util.Scanner;
+
+public class Q15_StringReversal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        String reversed = new StringBuilder(s).reverse().toString();
+
+        System.out.println("Reversed string: " + reversed);
+
+        sc.close();
+    }
+}
