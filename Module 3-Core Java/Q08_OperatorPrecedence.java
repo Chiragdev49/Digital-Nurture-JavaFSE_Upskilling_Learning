6af242704eac247723da76095405dbd78961a51f
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:51:49 +0530

    add file

--- Module 3-Core Java/Q08_OperatorPrecedence.java
+++ Module 3-Core Java/Q08_OperatorPrecedence.java
@@ -1,1 +1,12 @@
-public class Q08_OperatorPrecedence{public static void main(String[]a){System.out.println(10+5*2);}}
+public class Q08_OperatorPrecedence {
+    public static void main(String[] args) {
+
+        int result1 = 10 + 5 * 2;      
+        int result2 = (10 + 5) * 2;    
+        int result3 = 20 / 2 + 3 * 4;  
+
+        System.out.println("10 + 5 * 2 = " + result1);
+        System.out.println("(10 + 5) * 2 = " + result2);
+        System.out.println("20 / 2 + 3 * 4 = " + result3);
+    }
+}
