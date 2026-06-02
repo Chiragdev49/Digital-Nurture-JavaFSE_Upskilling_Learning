5ae2879b14ac768f113d1a4f8ea75ac540a35a07
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:50:33 +0530

    add file

--- Module 3-Core Java/Q10_NumberGuessingGame.java
+++ Module 3-Core Java/Q10_NumberGuessingGame.java
@@ -1,1 +1,32 @@
-import java.util.*; public class Q10_NumberGuessingGame{public static void main(String[]a){int r=new Random().nextInt(100)+1;Scanner s=new Scanner(System.in);int g;do{g=s.nextInt();System.out.println(g<r?"Low":g>r?"High":"Correct");}while(g!=r);}}
+import java.util.Random;
+import java.util.Scanner;
+
+public class Q10_NumberGuessingGame {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int r = rand.nextInt(100) + 1;
+
+        Scanner sc = new Scanner(System.in);
+
+        int g;
+
+        System.out.println("Guess the number between 1 and 100!");
+
+        do {
+            System.out.print("Enter your guess: ");
+            g = sc.nextInt();
+
+            if (g < r) {
+                System.out.println("Too Low!");
+            } else if (g > r) {
+                System.out.println("Too High!");
+            } else {
+                System.out.println("Correct! You guessed it.");
+            }
+
+        } while (g != r);
+
+        sc.close();
+    }
+}
