3467817f2d1ff60b0c71930a5007e33ef490e789
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:32:31 +0530

    add file

--- Module 3-Core Java/Q31_JDBCConnection.java
+++ Module 3-Core Java/Q31_JDBCConnection.java
@@ -1,1 +1,37 @@
-public class Q31_JDBCConnection{public static void main(String[]a){System.out.println("Configure DB and JDBC driver");}}
+import java.sql.*;
+
+public class Q31_JDBCConnection {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/testdb";
+        String user = "root";
+        String password = "root";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con =
+                    DriverManager.getConnection(url, user, password);
+
+            Statement stmt = con.createStatement();
+
+            ResultSet rs =
+                    stmt.executeQuery("SELECT * FROM students");
+
+            System.out.println("Student Records:");
+
+            while (rs.next()) {
+                System.out.println(
+                        rs.getInt("id") + " "
+                        + rs.getString("name") + " "
+                        + rs.getInt("age"));
+            }
+
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
