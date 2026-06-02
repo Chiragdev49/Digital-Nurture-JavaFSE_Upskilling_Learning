048a99f5787bcc631d7d723f4bcc4e145d1ddb1f
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:54:30 +0530

    add file

--- Module 3-Core Java/Q04_LeapYearChecker.java
+++ Module 3-Core Java/Q04_LeapYearChecker.java
@@ -1,1 +1,19 @@
-import java.util.*; public class Q04_LeapYearChecker{public static void main(String[]a){int y=new Scanner(System.in).nextInt();System.out.println((y%400==0||y%4==0&&y%100!=0)?"Leap":"Not Leap");}}
+import java.util.Scanner;
+
+public class Q04_LeapYearChecker {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int y = sc.nextInt();
+
+        if ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0)) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Not a Leap Year");
+        }
+
+        sc.close();
+    }
+}
