4965423614de037797f462b837318406298b07e1
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:19:56 +0530

    correction

--- Module 3-Core Java/Q37_BytecodeExample.java
+++ Module 3-Core Java/Q37_BytecodeExample.java
@@ -1,1 +1,11 @@
-public class Q37_BytecodeExample{void show(){System.out.println("javap demo");}}
+public class Q37_BytecodeExample {
+
+    void show() {
+        System.out.println("javap demo");
+    }
+
+    public static void main(String[] args) {
+        Q37_BytecodeExample obj = new Q37_BytecodeExample();
+        obj.show();
+    }
+}
