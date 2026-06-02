29d7a31c5531cf8b5072b7a947bec60b98bc239d
Chirag Dev <chiragdev128@gmail.com>
2026-06-02 19:43:06 +0530

    add file

--- Module 3-Core Java/Q21_CustomException.java
+++ Module 3-Core Java/Q21_CustomException.java
@@ -1,1 +1,26 @@
-public class Q21_CustomExceptionDemo{public static void main(String[]a)throws Exception{int age=15;if(age<18)throw new InvalidAgeException("Invalid Age");}}
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class Q21_CustomExceptionDemo {
+
+    public static void main(String[] args) {
+
+        int age = 15;
+
+        try {
+            if (age < 18) {
+                throw new InvalidAgeException(
+                        "Age must be 18 or above.");
+            }
+
+            System.out.println("Valid Age");
+
+        } catch (InvalidAgeException e) {
+            System.out.println("Exception Caught: "
+                    + e.getMessage());
+        }
+    }
+}
